0b19eccd27b714478d9d4f2a1d9344ad6bffb32d
maxorax <orlov3020max@gmail.com>
2019-12-22 16:07:00 +0400

    add HelloWorld

---
+++ src/LearnMain.java
@@ -0,0 +1,7 @@
+public class LearnMain {
+    public static void main(String[] args){
+        System.out.println("Hello Java");
+        System.out.println(13);
+        System.out.println("\n\tjava"+14);
+    }
+}
